38b2fb1b39bc741033403e3d6b8f5fd9cf4ea711
wangshuang <wangshuang@cqt2104081436+_>
2021-05-05 22:16:54 +0800

    添加切换画笔类型功能

--- src/public/page/whiteBoard/index.js
+++ src/public/page/whiteBoard/index.js
@@ -1,3 +1,30 @@
+"use strict";
+
 import FloatrayBoard from "../../library/floatRayBoard.js";
 
-const draw = new FloatrayBoard("#app");
+const draw = window.draw = new FloatrayBoard("#whiteBoardBox");
+
+[...document.querySelectorAll("ul.toolbar-operates > li")].forEach(el => {
+  el.addEventListener("click", function () {
+    const type = Number(this.dataset.type);
+    // 撤回
+    if (type === -3) {
+      return false;
+    }
+    // 删除
+    if (type === -2) {
+      return false;
+    }
+    // 切换工具选择状态
+    if (document.querySelector("ul.toolbar-operates > li.active")) {
+      document.querySelector("ul.toolbar-operates > li.active").classList.remove("active");
+    }
+    this.classList.add("active");
+    // 选择画笔路径
+    if (type === -1) {
+      return draw.setCanvasSelect(true);
+    }
+    // 设置画笔类型
+    return draw.setBrushType(type);
+  });
+});
